822548d52bb434d4256742609e691b83a8262232
Burak Kose <burakkose41@yahoo.com>
2014-03-26 13:52:06 +0200

    add new graph screen setting

--- src/graphicwindow.cpp
+++ src/graphicwindow.cpp
@@ -16,26 +16,46 @@ void GraphicWindow::setLand(Graph::vmap::const_iterator begin,
 
 void GraphicWindow::paintEvent(QPaintEvent *e){
     QPainter painter(this);
+    QPen penBegin , penLine;
+
+    penBegin.setStyle(Qt::DotLine);
+    penBegin.setColor(Qt::blue);
+    penLine.setStyle(Qt::SolidLine);
+    penLine.setColor(Qt::darkBlue);
+
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.setPen(penBegin);
+
     double x = land->centerScreen.cor_X;
     double y = land->centerScreen.cor_Y;
     double r = land->getBigRadius();
-    // painter.drawEllipse(QPoint(x,y),r,r);
+
+    painter.drawEllipse(QPoint(x,y),r,r);
+
+    painter.setPen(penLine);
+    painter.setBrush(Qt::darkRed);
+
     Graph::vmap* pVertexMap = &land->cVertexMap;
+
     for(dmap it = land->corMap.begin() ; it !=land->corMap.end();++it){
         QString nodeName = it.key();
-        double x = it.value()->cor_X;
-        double y = it.value()->cor_Y;
-        double r = land->getNodeRadius();
-        painter.drawEllipse(QPoint(x,y),r,r);
+
+        x = it.value()->cor_X;
+        y = it.value()->cor_Y;
+        r = land->getNodeRadius();
+
         for(QVector<Edge>::iterator ite = pVertexMap->value(nodeName)->adj.begin();
             ite != pVertexMap->value(nodeName)->adj.end();ite++){
+
             QString destNodeName = ite->destName;
-            //  if(it.value()->area != land->corMap.value(destNodeName)->area){
             double x2 = land->corMap.value(destNodeName)->cor_X;
             double y2 = land->corMap.value(destNodeName)->cor_Y;
-            painter.drawLine(QPoint(x,y),QPoint(x2,y2));
-            //  }
+            painter.drawLine(QPoint(x,y),QPoint(x2,y2)); // draw connection
+
         }
+
+        painter.drawEllipse(QPoint(x,y),r,r); //draw node
+        //painter.drawText(QPoint(x,y),nodeName);
     }
 }
 
